f0b5de5795f08960c8d9465222934f9a8e09d336
Dave Edwards <EdwardsD@hlcollege.ac.uk>
2022-11-17 15:11:32 +0000

    Add project files.

---
+++ noughts_and_crosses.py
@@ -0,0 +1,31 @@
+grid = [[' ',' ',' '],
+        [' ',' ',' '],
+        [' ',' ',' ']]
+players = ['O', 'X']
+next_player = 0
+
+grid[0][0] = players[1]
+grid[2][2] = players[0]
+grid[2][0] = players[1]
+grid[1][0] = players[0]
+grid[0][2] = players[1]
+grid[0][1] = players[0]
+grid[1][1] = players[1]
+
+def get_next_character():
+    """Returns the character for the next player"""
+    global players
+    global next_player
+    #Get the next character
+    next_character = players[next_player]
+    #Update next_player
+    if next_player == 1:
+        next_player = 0
+    elif next_player == 0:
+        next_player = 1
+    else:
+        print("Prob in get_next_character")
+    return next_player
+#test function
+for i in range(9):
+    print(get_next_character())
